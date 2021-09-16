42bf48a1c10aa590fb443f25e965d2e684f2def5
tee1365 <justin951124@gmail.com>
2021-09-17 02:24:05 +1200

    update

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,18 +1,27 @@
+import { ApolloClient, ApolloProvider, InMemoryCache } from '@apollo/client';
 import { ChakraProvider, ColorModeProvider } from '@chakra-ui/react';
 import React from 'react';
 import theme from '../theme';
 
+const client = new ApolloClient({
+  uri: 'http://localhost:4000/graphql',
+  cache: new InMemoryCache(),
+  credentials: 'include',
+});
+
 const MyApp = ({ Component, pageProps }: any): JSX.Element => {
   return (
-    <ChakraProvider resetCSS theme={theme}>
-      <ColorModeProvider
-        options={{
-          useSystemColorMode: true,
-        }}
-      >
-        <Component {...pageProps} />
-      </ColorModeProvider>
-    </ChakraProvider>
+    <ApolloProvider client={client}>
+      <ChakraProvider resetCSS theme={theme}>
+        <ColorModeProvider
+          options={{
+            useSystemColorMode: true,
+          }}
+        >
+          <Component {...pageProps} />
+        </ColorModeProvider>
+      </ChakraProvider>
+    </ApolloProvider>
   );
 };
 
